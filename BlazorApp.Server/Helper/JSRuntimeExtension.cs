07cf4efd900f2aa07d01b3a330b22319a564fbf8
Martin <martin-one2@hotmail.com>
2022-08-12 13:51:36 -0500

    Section 5: End

---
+++ BlazorApp.Server/Helper/JSRuntimeExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorApp.Server.Helper
+{
+    public static class JSRuntimeExtension
+    {
+        public static async ValueTask ShowToastr(this IJSRuntime jsRuntime, string message, string type)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", type, message);
+        }
+    }
+}
